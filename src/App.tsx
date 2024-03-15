99255fd8fdab4c5a6aedc5cc1350ba7fd231b216
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2024-03-15 13:33:33 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-//importando css, './' = mesma pasta
-//import "./App.css";
+// //importando css, './' = mesma pasta
+// //import "./App.css";
 
-// Tipagem necessaria em tsx, any = var(qualquer)
-// MeuBotao vira uma tag
-const MeuBotao = (props : any) =>{
-  // props indica acesso as propriedas
-  return( 
-    // O que a tag MeuBotao vai inserir na tela
-    //titulo é a propriedade em si
-    <button> {props.titulo}</button>
-  );
-}
+// // Tipagem necessaria em tsx, any = var(qualquer)
+// // MeuBotao vira uma tag
+// const MeuBotao = (props : any) =>{
+//   // props indica acesso as propriedas
+//   return( 
+//     // O que a tag MeuBotao vai inserir na tela
+//     //titulo é a propriedade em si
+//     <button> {props.titulo}</button>
+//   );
+// }
 
-const App = () => {
-    return (
-      //uma tag pai é obrigatoria, nesse caso é o div, todo tqm que ser feito dentro dele
-      //className é parte da sintxe do jsx
-      <div className="aplicacao">
-        <h1>Bem vindo ao mundo React</h1>
-        <button>eu sou um botão html</button>
-        <MeuBotao titulo="Sua mae" />
-        <MeuBotao titulo="Seu pai" />
-        <MeuBotao titulo="Você" />
-      </div>
-    );
-  }
+// const App = () => {
+//     return (
+//       //uma tag pai é obrigatoria, nesse caso é o div, todo tqm que ser feito dentro dele
+//       //className é parte da sintxe do jsx
+//       <div className="aplicacao">
+//         <h1>Bem vindo ao mundo React</h1>
+//         <button>eu sou um botão html</button>
+//         <MeuBotao titulo="Sua mae" />
+//         <MeuBotao titulo="Seu pai" />
+//         <MeuBotao titulo="Você" />
+//       </div>
+//     );
+//   }
   
-//jsx = React
-export default App
+// //jsx = React
+// export default App
