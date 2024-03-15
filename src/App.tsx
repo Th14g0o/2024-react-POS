52daa7dc9ed3b2ea3f419d31c2abdb9db927beae
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2024-03-15 13:44:30 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -18,30 +18,6 @@ let TarefaAppBar = () => {
   );
 }
 
-let Tarefa = (props: any) => {
-  return (
-    <li class="p-2 rounded-lg">
-      <div class="flex align-middle flex-row justify-between">
-        <div class="p-2">
-          <input type="checkbox" class="h-6 w-6 " checked={props.data.concluido}   />
-        </div>
-        <div class="p-2">
-          <p class="text-lg text-black" >  {props.data.titulo} </p>
-        </div>
-        <button class="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
-          <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-            stroke-linecap="round" stroke-linejoin="round">
-            <circle cx="12" cy="12" r="10" />
-            <line x1="15" y1="9" x2="9" y2="15" />
-            <line x1="9" y1="9" x2="15" y2="15" /></svg>
-          <span>Remove</span>
-        </button>
-      </div>
-      <hr class="mt-2" />
-    </li>
-  )
-}
-
 const App = () => {
   // let obj_tarefas = tarefas.map((item, indice) => { return { id: indice, titulo: item } });
   return (
@@ -51,7 +27,25 @@ const App = () => {
           <TarefaAppBar />
           <div class="mt-8">
             <ul>
-             
+              <li class="p-2 rounded-lg">
+                <div class="flex align-middle flex-row justify-between">
+                  <div class="p-2">
+                    <input type="checkbox" class="h-6 w-6 " checked={props.data.concluido} />
+                  </div>
+                  <div class="p-2">
+                    <p class="text-lg text-black" >  {props.data.titulo} </p>
+                  </div>
+                  <button class="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
+                    <svg class="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                      stroke-linecap="round" stroke-linejoin="round">
+                      <circle cx="12" cy="12" r="10" />
+                      <line x1="15" y1="9" x2="9" y2="15" />
+                      <line x1="9" y1="9" x2="15" y2="15" /></svg>
+                    <span>Remove</span>
+                  </button>
+                </div>
+                <hr class="mt-2" />
+              </li>
             </ul>
           </div>
           <div class="mt-8">
@@ -90,6 +84,6 @@ const App = () => {
 //       </div>
 //     );
 //   }
-  
+
 // //jsx = React
 export default App
