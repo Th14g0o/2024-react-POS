96d8e81dfeb32cfbe1487348afd0a1a4613c3888
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2024-03-15 13:57:48 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -29,10 +29,7 @@ const Tarefa = (props: any) => {
           <p className="text-lg text-black" >  {props.data.titulo} </p>
         </div>
         <button className="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
-          <svg className="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <circle cx="12" cy="12" r="10" />
-            <line x1="15" y1="9" x2="9" y2="15" />
-            <line x1="9" y1="9" x2="15" y2="15" /></svg>
+          
           <span>Remove</span>
         </button>
       </div>
