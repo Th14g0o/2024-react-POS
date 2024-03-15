c148d634916429d0fdd5d83176e0163ccf889087
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2024-03-15 13:53:20 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,61 @@
+let TarefaAppBar = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Tarefas do Professor</h1>
+      <div className="mt-4 flex">
+        <input className="w-80 border-b-2 border-gray-500 text-black px-2" type="text" placeholder="informe a tarefa" />
+        <button className="ml-2 border-2 border-green-500 p-2 text-green-500 hover:text-white hover:bg-green-500 rounded-lg flex">
+          <svg className="h-6 w-6" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+            fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <circle cx="12" cy="12" r="9" />
+            <line x1="9" y1="12" x2="15" y2="12" />
+            <line x1="12" y1="9" x2="12" y2="15" /></svg>
+          <span>Adicionar</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+let Tarefa = (props: any) => {
+  return (
+    <li className="p-2 rounded-lg">
+      <div className="flex align-middle flex-row justify-between">
+        <div className="p-2">
+          <input type="checkbox" className="h-6 w-6 " checked={props.data.concluido}   />
+        </div>
+        <div className="p-2">
+          <p className="text-lg text-black" >  {props.data.titulo} </p>
+        </div>
+        <button className="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
+          <svg className="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="12" cy="12" r="10" />
+            <line x1="15" y1="9" x2="9" y2="15" />
+            <line x1="9" y1="9" x2="15" y2="15" /></svg>
+          <span>Remove</span>
+        </button>
+      </div>
+      <hr className="mt-2" />
+    </li>
+  )
+}
 const App = () => {
-  // let obj_tarefas = tarefas.map((item, indice) => { return { id: indice, titulo: item } });
+  const tarefas = [
+    {titulo: "Coisa 1", concluido: false},
+    {titulo: "Coisa 2", concluido: true},
+    {titulo: "Coisa 3", concluido: false}
+  ];
+  let mapa = tarefas.map((item) => (<li>{item}</li>));
   return (
     <>
       <div className="w-full h-screen bg-gray-100 pt-8 ">
         <div className="bg-white p-3 max-w-md mx-auto">
-          <div className="text-center">
-            <h1 className="text-3xl font-bold">Tarefas do Professor</h1>
-            <div className="mt-4 flex">
-              <input className="w-80 border-b-2 border-gray-500 text-black px-2" type="text" placeholder="informe a tarefa" />
-              <button className="ml-2 border-2 border-green-500 p-2 text-green-500 hover:text-white hover:bg-green-500 rounded-lg flex">
-               
-                <span>Adicionar</span>
-              </button>
-            </div>
-          </div>
+         <TarefaAppBar />
           <div className="mt-8">
             <ul>
-              <li className="p-2 rounded-lg">
-                <div className="flex align-middle flex-row justify-between">
-                  <div className="p-2">
-                    <input type="checkbox" className="h-6 w-6 "  />
-                  </div>
-                  <div className="p-2">
-                    <p className="text-lg text-black" >  Tarefa</p>
-                  </div>
-                  <button className="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
-                    <svg className="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                      stroke-linecap="round" stroke-linejoin="round">
-                      <circle cx="12" cy="12" r="10" />
-                      <line x1="15" y1="9" x2="9" y2="15" />
-                      <line x1="9" y1="9" x2="15" y2="15" /></svg>
-                    <span>Remove</span>
-                  </button>
-                </div>
-                <hr className="mt-2" />
-              </li>
+            {tarefas.map((tarefa) => (<Tarefa data={tarefa} />))}
             </ul>
           </div>
           <div className="mt-8">
