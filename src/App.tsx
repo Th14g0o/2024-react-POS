04fd631118271f7e66de90721f29a59b6517b642
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2024-03-15 14:09:12 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ let TarefaAppBar = () => {
       <div className="mt-4 flex">
         <input className="w-80 border-b-2 border-gray-500 text-black px-2" type="text" placeholder="informe a tarefa" />
         <button className="ml-2 border-2 border-green-500 p-2 text-green-500 hover:text-white hover:bg-green-500 rounded-lg flex">
-          <svg className="h-6 w-6" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-            fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg className="h-6 w-6" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" />
             <circle cx="12" cy="12" r="9" />
             <line x1="9" y1="12" x2="15" y2="12" />
-            <line x1="12" y1="9" x2="12" y2="15" /></svg>
+            <line x1="12" y1="9" x2="12" y2="15" />
+          </svg>
           <span>Adicionar</span>
         </button>
       </div>
@@ -18,47 +18,24 @@ let TarefaAppBar = () => {
   );
 }
 
-let Tarefa = (props: any) => {
-  return (
-    <li className="p-2 rounded-lg">
-      <div className="flex align-middle flex-row justify-between">
-        <div className="p-2">
-          <input type="checkbox" className="h-6 w-6 " checked={props.data.concluido} />
-        </div>
-        <div className="p-2">
-          <p className="text-lg text-black" >  {props.data.titulo} </p>
-        </div>
-        <button className="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
-          
-          <span>Remove</span>
-        </button>
-      </div>
-      <hr className="mt-2" />
-    </li>
-  );
-}
-function  App (){
-  const tarefas = [
-    {titulo: "Coisa 1", concluido: false},
-    {titulo: "Coisa 2", concluido: true},
-    {titulo: "Coisa 3", concluido: false}
-  ];
-  let mapa = tarefas.map((item) => (<li>{item}</li>));
-  return (
-      <div className="w-full h-screen bg-gray-100 pt-8 ">
-        <div className="bg-white p-3 max-w-md mx-auto">
-          <TarefaAppBar />
-          <div className="mt-8">
-            <ul>
 
-            </ul>
-          </div>
-          <div className="mt-8">
-            <button className="border-2 border-red-500 p-2 text-red-500">Limpar tarefas completadas</button>
-            <button className="border-2 border-indigo-500 p-2 text-indigo-500 ml-4">Resetar lista de afazares</button>
-          </div>
+const App = () => {
+  
+  return (
+    <div className="w-full h-screen bg-gray-100 pt-8 ">
+      <div className="bg-white p-3 max-w-md mx-auto">
+        <TarefaAppBar />
+        <div className="mt-8">
+          <ul>
+           
+          </ul>
+        </div>
+        <div className="mt-8">
+          <button className="border-2 border-red-500 p-2 text-red-500">Limpar tarefas completadas</button>
+          <button className="border-2 border-indigo-500 p-2 text-indigo-500 ml-4">Resetar lista de afazares</button>
         </div>
       </div>
+    </div>
   );
 }
 // //importando css, './' = mesma pasta
