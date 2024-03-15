cf92217ba85a81213ce676606e2653d0d749a330
Thiago Gonsalves <93939243+Th14g0o@users.noreply.github.com>
2024-03-15 14:13:33 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,28 @@ let TarefaAppBar = () => {
   );
 }
 
+const Tarefa = (props: any) => {
+  return (
+    <li className="p-2 rounded-lg">
+      <div className="flex align-middle flex-row justify-between">
+        <div className="p-2">
+          <input type="checkbox" className="h-6 w-6 " checked={props.data.concluido} />
+        </div>
+        <div className="p-2">
+          <p className="text-lg text-black" >  {props.data.titulo} </p>
+        </div>
+        <button className="flex self-center h-min text-red-500 border-2 border-red-500 p-2 rounded-lg">
+          <svg className="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="12" cy="12" r="10" />
+            <line x1="15" y1="9" x2="9" y2="15" />
+            <line x1="9" y1="9" x2="15" y2="15" /></svg>
+          <span>Remove</span>
+        </button>
+      </div>
+      <hr className="mt-2" />
+    </li>
+  );
+}
 
 const App = () => {
   const tarefas = [
@@ -25,14 +47,13 @@ const App = () => {
     { titulo: "Coisa 2", concluido: true },
     { titulo: "Coisa 3", concluido: false }
   ];
-  let mapa = tarefas.map((item) => (<li>{item}</li>));
+
   return (
     <div className="w-full h-screen bg-gray-100 pt-8 ">
       <div className="bg-white p-3 max-w-md mx-auto">
         <TarefaAppBar />
         <div className="mt-8">
           <ul>
-
           </ul>
         </div>
         <div className="mt-8">
